ac28397bc75e192df78fd1846164f5b537fb0b21
Clément <cnicole@ensc.fr>
2025-02-12 14:55:11 +0100

    UtilisateurController ok

--- ApiBazarDesSport/Controllers/ReservationController.cs
+++ ApiBazarDesSport/Controllers/ReservationController.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 [ApiController]
-[Route("api/Reservation")]
+[Route("api/Reservations")]
 public class Reservations : ControllerBase
 {
     private readonly BdsContexte _context;
--- ApiBazarDesSport/Controllers/UtilisateurController.cs
+++ ApiBazarDesSport/Controllers/UtilisateurController.cs
@@ -2,60 +2,60 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 [ApiController]
-[Route("api/Materiels")]
-public class Materiels : ControllerBase
+[Route("api/Utilisateurs")]
+public class Utilisateurs : ControllerBase
 {
     private readonly BdsContexte _context;
 
-    public Materiels(BdsContexte context)
+    public Utilisateurs(BdsContexte context)
     {
         _context = context;
     }
 
-    // GET: api/Materiel
+    // GET: api/Reservation
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<Materiel>>> GetMateriel()
+    public async Task<ActionResult<IEnumerable<Reservation>>> GetReservation()
     {
         // Get Item
-        var Materiels = _context.Materiel;
-        return await Materiels.ToListAsync();
+        var Utilisateur = _context.Reservation;
+        return await Utilisateur.ToListAsync();
     }
 
-    // GET: api/Materiel/{id}
+    // GET: api/Reservation/{id}
     [HttpGet("{id}")]
-    public async Task<ActionResult<Materiel>> GetItem(int id)
+    public async Task<ActionResult<Reservation>> GetItem(int id)
     {
         // Find a specific personne
         // SingleAsync() throws an exception if no personne is found (which is possible, depending on id)
         // SingleOrDefaultAsync() is a safer choice here
-        var Materiel = await _context.Materiel.SingleOrDefaultAsync(t => t.Id == id);
-        if (Materiel == null)
+        var Reservation = await _context.Reservation.SingleOrDefaultAsync(t => t.Id == id);
+        if (Reservation == null)
             return NotFound();
-        return Materiel;
+        return Reservation;
     }
 
-    // POST: api/Materiel
+    // POST: api/Reservation
     [HttpPost]
-    public async Task<ActionResult<Materiel>> PostItem(Materiel item)
+    public async Task<ActionResult<Reservation>> PostItem(Reservation item)
     {
         /*if (!new[] { "mobilier", "bureautique", "électronique" }.Contains(item.Type))
         {
-            return BadRequest("Type de Materiel invalide.");
+            return BadRequest("Type de Reservation invalide.");
         }*/
-        _context.Materiel.Add(item);
+        _context.Reservation.Add(item);
         await _context.SaveChangesAsync();
         return CreatedAtAction(nameof(GetItem), new { id = item.Id }, item);
     }
 
-    // PUT: api/Materiel/{id}
+    // PUT: api/Reservation/{id}
     [HttpPut("{id}")]
-    public async Task<IActionResult> PutItem(int id, Materiel item)
+    public async Task<IActionResult> PutItem(int id, Reservation item)
     {
         if (id != item.Id)
             return BadRequest();
         /*if (!new[] { "mobilier", "bureautique", "électronique" }.Contains(item.Type))
         {
-            return BadRequest("Type de Materiel invalide.");
+            return BadRequest("Type de Reservation invalide.");
         }*/
         _context.Entry(item).State = EntityState.Modified;
         try
@@ -64,7 +64,7 @@ public class Materiels : ControllerBase
         }
         catch (DbUpdateConcurrencyException)
         {
-            if (!_context.Materiel.Any(m => m.Id == id))
+            if (!_context.Reservation.Any(m => m.Id == id))
                 return NotFound();
             else
                 throw;
@@ -72,14 +72,14 @@ public class Materiels : ControllerBase
         return NoContent();
     }
 
-    // DELETE: api/Materiel/{id}
+    // DELETE: api/Reservation/{id}
     [HttpDelete("{id}")]
     public async Task<IActionResult> DeleteItem(int id)
     {
-        var item = await _context.Materiel.FindAsync(id);
+        var item = await _context.Reservation.FindAsync(id);
         if (item == null)
             return NotFound();
-        _context.Materiel.Remove(item);
+        _context.Reservation.Remove(item);
         await _context.SaveChangesAsync();
         return NoContent();
     }
